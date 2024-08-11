55bb1c25a6ca80c5d73e85fe33843f23cc05fd2f
Lorepelhu98 <lorenzopelhuri@libero.it>
2024-08-11 18:04:59 +0200

    coomit finale

--- frontend/src/app/recipe/recipe.component.ts
+++ frontend/src/app/recipe/recipe.component.ts
@@ -6,6 +6,7 @@ interface Recipe {
   id: number;
   name: string;
   description: string;
+  imageUrl?: string;
 }
 
 
@@ -16,11 +17,11 @@ interface Recipe {
 })
 export class RecipeComponent implements OnInit {
   recipes: Recipe[] = [
-    { id: 1, name: 'Spaghetti al Pomodoro', description: 'Un classico della cucina italiana, preparato con pomodori freschi, basilico e un tocco di olio d\'oliva.' },
-    { id: 2, name: 'Insalata Caprese', description: 'Un piatto fresco e leggero con pomodori, mozzarella di bufala, basilico e un filo di olio d\'oliva.' },
-    { id: 3, name: 'Lasagna Vegetariana', description: 'Una deliziosa lasagna con strati di verdure miste, ricoperta di salsa di pomodoro e formaggio.' },
-    { id: 4, name: 'Ratatouille', description: 'Uno stufato di verdure di origine francese, preparato con zucchine, melanzane, peperoni e pomodori.' },
-    { id: 5, name: 'Curry di Ceci', description: 'Un piatto ricco e speziato con ceci, pomodori, latte di cocco e una miscela di spezie aromatiche.' }
+    { id: 1, name: 'Spaghetti al Pomodoro', description: 'Un classico della cucina italiana, preparato con pomodori freschi, basilico e un tocco di olio d\'oliva.', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/2a/Spaghetti_al_Pomodoro.JPG' },
+    { id: 2, name: 'Insalata Caprese', description: 'Un piatto fresco e leggero con pomodori, mozzarella di bufala, basilico e un filo di olio d\'oliva.', imageUrl:'https://th.bing.com/th?id=OSK.c87b1fb5d0756857357a04f2c3ccc438&w=164&h=93&rs=2&qlt=80&o=6&cdv=1&dpr=1.3&pid=16.1' },
+    { id: 3, name: 'Lasagna Vegetariana', description: 'Una deliziosa lasagna con strati di verdure miste, ricoperta di salsa di pomodoro e formaggio.',  imageUrl:'https://misrecetasdecocina.net/wp-content/uploads/2017/09/lasa%C3%B1a-vegetariana.jpg' },
+    { id: 4, name: 'Ratatouille', description: 'Uno stufato di verdure di origine francese, preparato con zucchine, melanzane, peperoni e pomodori.',  imageUrl:'https://www.liveeatlearn.com/wp-content/uploads/2017/01/ratatouille-vegetable-tian-6.jpg' },
+    { id: 5, name: 'Curry di Ceci', description: 'Un piatto ricco e speziato con ceci, pomodori, latte di cocco e una miscela di spezie aromatiche.',  imageUrl:'https://blog.giallozafferano.it/ricettandocondany/wp-content/uploads/2023/05/EOS-6D-Mark-II_7048ott.jpg' },
   ];
 
   view: string = 'list';
@@ -44,13 +45,11 @@ export class RecipeComponent implements OnInit {
 
   onSubmit(): void {
     if (this.editMode) {
-      // Update recipe logic
       const index = this.recipes.findIndex(r => r.id === this.recipeForm.id);
       if (index !== -1) {
         this.recipes[index] = this.recipeForm;
       }
     } else {
-      // Add new recipe logic
       this.recipes.push(this.recipeForm);
     }
     this.view = 'list';
